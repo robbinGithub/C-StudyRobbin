2713d199fa3295d15b68ee47662d6d7fbc29222a
robbin2117 <robbin2117@163.com>
2018-08-27 18:25:23 +0800

    更新
    Former-commit-id: c7d6ecd31c1b1a4f9b723fd9a000423d93aa49b4

--- C++ 智能指针/demo02_shared_ptr引用计数,增加和减少的规律.cpp
+++ C++ 智能指针/demo02_shared_ptr引用计数,增加和减少的规律.cpp
@@ -2,6 +2,11 @@
 #include <memory>
 using namespace std;
 
+/*
+ https://blog.csdn.net/haolipengzhanshen/article/details/52205102
+ https://www.cnblogs.com/xiehongfeng100/p/4645555.html
+ https://www.cnblogs.com/wxquare/p/4759020.html
+*/
 void Func1(shared_ptr<int> a)
 {
 	cout << "Enter Func1" << endl;
@@ -17,7 +22,7 @@ shared_ptr<int> Func2(shared_ptr<int>& a)
 	return a;
 }
 
-int main()
+int main_02()
 {
 	//构造一个指向int类型对象的指针aObj1,引用计数+1
 	shared_ptr<int> aObj1(new int(10));
@@ -29,11 +34,17 @@ int main()
 		cout << "Ref count: " << aObj2.use_count() << endl;//引用计数-1
 	}
 
+	cout << "T1_Ref count: " << aObj1.use_count() << endl;
+
 	//在调用函数时,参数为shared_ptr类型,参数为传值类型,智能指针引用计数+1
 	Func1(aObj1);
 
+	cout << "T2_Ref count: " << aObj1.use_count() << endl;
+
 	//在调用函数时,参数为shared_ptr类型,参数为传引用类型,智能指针引用计数不变
 	Func2(aObj1);
+
+	cout << "T3_Ref count: " << aObj1.use_count() << endl;
 
 	shared_ptr<int> aObj3 = Func2(aObj1);//引用计数+1
 	cout << "Ref count:" << aObj3.use_count() << endl;
