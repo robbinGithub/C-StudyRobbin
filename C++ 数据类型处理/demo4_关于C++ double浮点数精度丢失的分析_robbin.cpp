60e81461e5200dc71aeaf7463707e317926ddab3
robbin2117 <robbin2117@163.com>
2018-09-04 18:15:19 +0800

    last submit
    Former-commit-id: 793f780ae8918597ea70f71f2de56ebbd7d85b93

--- C++ 数据类型处理/demo4_关于C++ double浮点数精度丢失的分析_robbin.cpp
+++ C++ 数据类型处理/demo4_关于C++ double浮点数精度丢失的分析_robbin.cpp
@@ -4,6 +4,7 @@
  *
  * @see https://www.cnblogs.com/c-primer/p/5992696.html
  *      https://blog.csdn.net/cai870808/article/details/24907853
+ *      http://blog.chinaunix.net/uid-26748719-id-3316174.html
  */
 int main()
 {
@@ -13,6 +14,8 @@ int main()
 	printf("%s\n", a == b ? "true" : "false");
 
 	float f = 1.2;
+	float f2 = 8388608.12;
+	float f3 = 838860.12;
 
 	system("pause");
 }
