ce4958be8820443149da749680d3c43f6f3c2ff1
robbin2117 <robbin2117@163.com>
2018-09-04 18:37:42 +0800

    aa
    Former-commit-id: a67ad94bf29c9f2e8b29530612f1e592b8545f4a

--- C++ 数据类型处理/demo4_关于C++ double浮点数精度丢失的分析_robbin.cpp
+++ C++ 数据类型处理/demo4_关于C++ double浮点数精度丢失的分析_robbin.cpp
@@ -13,9 +13,23 @@ int main()
 	long b = db;
 	printf("%s\n", a == b ? "true" : "false");
 
-	float f = 1.2;
+	float f = -1.2;
 	float f2 = 8388608.12;
 	float f3 = 838860.12;
+	float f4 = 83.8860812;
+
+	printf("f2 + 1:%f \n", f2 + 1); // 8388609.000000
+	printf("f3 + 1:%f \n", f3 + 1); // 838861.125000
+
+	printf("f1 address:%p\n", &f);
+	printf("f4 address:%p\n", &f4);
+
+	// f4: acc5a742
+	// 1bit（符号位） 8bits（指数位）   23bits（尾数位）
+    // 1              01011001          10001011010011101000010
+
+	// f1
+	// 1 00110101 00110011001100100111111
 
 	system("pause");
 }
